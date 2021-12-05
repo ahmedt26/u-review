d2f8c630a740e1734f81f2a3001d482dd0d5cc98
Tahseen Ahmed <ahmedt26@mcmaster.ca>
2021-12-05 03:18:41 -0500

    Changed db to use port.

--- database.php
+++ database.php
@@ -1,20 +1,21 @@
 <?php
+// This template code was taken from Workshop 9 and modified for UReview use.
 class Database
 {
 
-    // specify your own database credentials
-    private $host = "localhost";
+    // Database Credentials
+    private $host = "localhost:3306";
     private $db = "ureview";
     private $username = "root";
     private $password = "TestPa*ss21";
     public $conn;
-    // get the database connection
+    // Establish Database Connection
     public function getConnection()
     {
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db);
         if ($this->conn->connect_error) {
             $response['status'] = '0';
-            $response['message'] = $exception->getMessage();
+            // $response['message'] = $exception->getMessage();
             return $response;
         } else {
             $response['status'] = '1';
