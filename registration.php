16657d4fd54ec229555cfbba09ba73436e35f743
Tahseen Ahmed <ahmedt26@mcmaster.ca>
2021-12-05 02:47:15 -0500

    testing adding to db.

--- registration.php
+++ registration.php
@@ -43,8 +43,11 @@
   <?php include('header.html'); ?>
   <?php
   // Check if form is submitted with filled values, then POST data.
-  if (isset($_POST['form_submitted']) && isset($_POST['username']) && isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['userEmail']) && isset($_POST['userEmail'])) :
+  if (isset($_POST['form_submitted'])) :
   ?>
+    <script type="text/JavaScript">
+      validateRegistration();
+      <script>
     <h2> Welcome to UReview, <?php echo $_POST['username']; ?> ! </h2>
 
     <p>You have been registered as
