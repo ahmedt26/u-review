a0ae5d1cf64abc6c6e2fb43cbac9dbd5546b8081
Tahseen Ahmed <ahmedt26@mcmaster.ca>
2021-12-08 13:39:50 -0500

    Forgot percent sign for search term.

--- get_locations.php
+++ get_locations.php
@@ -14,7 +14,7 @@ if ($db['status'] == '0') {
 $search_term = "";
 if (isset($_GET['search']) && $_GET['search'] != '') {
     $search = $_GET['search'];
-    $search_term = "%$search";
+    $search_term = "%$search%";
 } else {
     $search_term = "%";
 }
